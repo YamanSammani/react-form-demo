06961d134893c0be369cc40cd7c1602726c98b34
halit <halitfuatbatur@gmail.com>
2020-09-15 18:34:48 +0300

    form demo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  // const [firstName, setFirstName] = React.useState("");
+  // const [lastName, setLastName] = React.useState("");
+  // const [age, setAge] = React.useState("");
+  const [userInput, setUserInput] = React.useState({
+    firstName: "",
+    lastName: "",
+    age: "",
+  });
+
+  // const handleFirstNameChange = (e) => {
+  //   console.log(e.target.value);
+  //   setFirstName(e.target.value);
+  // };
+
+  const handleInputChange = (e, key) => {
+    setUserInput({ ...userInput, [key]: e.target.value });
+    console.log(userInput);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`
+    First Name : ${userInput.firstName}
+    Last Name : ${userInput.lastName}
+    Age : ${userInput.age}
+    `);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="text"
+          value={userInput.firstName}
+          onChange={(e) => handleInputChange(e, "firstName")}
+          placeholder="First Name"
+        />
+        <input
+          type="text"
+          onChange={handleInputChange}
+          value={userInput.lastName}
+          onChange={(e) => handleInputChange(e, "lastName")}
+          placeholder="Last Name"
+        />
+        <input
+          type="number"
+          onChange={handleInputChange}
+          value={userInput.age}
+          onChange={(e) => handleInputChange(e, "age")}
+          placeholder="Age"
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
